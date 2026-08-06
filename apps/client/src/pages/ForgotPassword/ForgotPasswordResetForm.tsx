637727c3510760288a2c8171d41178a436d849db
JANG <greedf@naver.com>
2026-08-06 21:02:56 +0900

    refactor: 상단 바 제거

--- apps/client/src/pages/ForgotPassword/ForgotPasswordResetForm.tsx
+++ apps/client/src/pages/ForgotPassword/ForgotPasswordResetForm.tsx
@@ -24,10 +24,7 @@ export default function ResetPasswordFormContent() {
   return (
     <S.Container>
       <S.Header>
-        <S.StatusBarMock>
-          <span>9:41</span>
-          <span>•••</span>
-        </S.StatusBarMock>
+        <S.StatusBarMock></S.StatusBarMock>
 
         <S.NavBar>
           <S.BackButton type="button" onClick={() => navigate(-1)}>
